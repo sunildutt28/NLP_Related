6247e43b735133aa9a75557e23fc0ac64eeebd03
Sunil Dutt <sunil.dutt28@gmail.com>
2025-08-25 22:02:13 +0100

    requirements.txt

--- SentimentAnalysis_App.py
+++ SentimentAnalysis_App.py
@@ -8,7 +8,7 @@ st.write("Classify text as Positive, Negative, or Neutral using Hugging Face Tra
 # Load model once
 @st.cache_resource
 def load_model():
-    return pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+    return pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment") #for +ve or -ve = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 
 analyzer = load_model()
 
@@ -24,13 +24,13 @@ if st.button("Analyze Sentiment"):
             score = round(result['score'], 3)
 
             st.subheader("📌 Sentiment Result")
-            st.write(f"**Label:** {label}")
+            #st.write(f"**Label:** {label}") # to find the label
             st.write(f"**Confidence:** {score}")
             
             # Add emoji indicator
-            if label == "POSITIVE":
+            if label == "LABEL_2":
                 st.success("😀 Positive Sentiment")
-            elif label == "NEGATIVE":
+            elif label == "LABEL_0":
                 st.error("😞 Negative Sentiment")
             else:
                 st.info("😐 Neutral Sentiment")
